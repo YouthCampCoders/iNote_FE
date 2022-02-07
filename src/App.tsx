324967ae3f5eaba951ce7bd852b82db286b3e763
Chuck <ml28888@126.com>
2022-02-07 22:07:35 +0800

    Merge dev-dk with dev-gks

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './store'
 
 // 组件
 import PageNotify from 'pages/PageNotify'
 import PageHome from 'pages/PageHome/PageHome'
-import PageNotFound from 'pages/PageNotFound'
 import PageFaq from 'pages/PageFaq'
 import PageSetting from 'pages/PageSetting'
 import PageMyNote from 'pages/PageMyNote'
 import CpnNavBar from 'components/CpnNavBar/CpnNavBar'
+import PageNotFound from 'pages/PageNotFound'
 import PageFirst from 'pages/PageFirst'
+
 
 const App: React.FC = (props) => {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <CpnNavBar />
-        <Routes>
-          <Route path="/" element={<Navigate to={'/first'} />} />
-          <Route path="/first" element={<PageFirst />} />
-          <Route path="/home" element={<PageHome />} />
-          <Route path="/notify" element={<PageNotify />} />
-          <Route path="/faq" element={<PageFaq />} />
-          <Route path="/setting" element={<PageSetting />} />
-          <Route path="/mynote" element={<PageMyNote />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <CpnNavBar />
+      <Routes>
+        <Route path="/" element={<Navigate to={'/home'} />} />
+        <Route path="/notify" element={<PageNotify />} />
+        <Route path="/home" element={<PageHome />} />
+        <Route path="/faq" element={<PageFaq/>}/>
+        <Route path="/setting" element={<PageSetting/>}/>
+        <Route path="/mynote" element={<PageMyNote />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
